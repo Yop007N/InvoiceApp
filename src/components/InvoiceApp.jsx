ce7c83a108113f718c4a901a581d082c3bb115d2
Yop007N <115731216+Yop007N@users.noreply.github.com>
2024-01-16 20:35:15 -0300

    Primer commit: Inicializando proyecto de InvoiceApp

---
+++ src/components/InvoiceApp.jsx
@@ -0,0 +1,13 @@
+
+const InvoiceApp = () => {
+  // Tu lógica y contenido del componente aquí
+
+  return (
+    <div>
+      <h1>InvoiceApp Component</h1>
+      {/* Agrega tu contenido aquí */}
+    </div>
+  );
+};
+
+export default InvoiceApp;
